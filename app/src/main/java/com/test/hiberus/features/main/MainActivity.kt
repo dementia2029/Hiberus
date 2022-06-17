7fc319fbebc4e0894d3aa84e9a89699f0919efe9
vadym.shved <vadym.shved@chisw.com>
2022-06-17 06:40:48 +0300

    nav component

--- app/src/main/java/com/test/hiberus/features/main/MainActivity.kt
+++ app/src/main/java/com/test/hiberus/features/main/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.test.hiberus.features.main
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import com.test.hiberus.R
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main)
